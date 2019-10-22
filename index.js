1c87fccff4954990414cb0e3b6c0f9f8918e2486
yish lim <yish.lim@flatironschool.com>
2019-10-22 19:05:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+  let greeting
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  }
+  if (feet > 2000) {
+    greeting = 'I will gladly take your thirty bucks.';
+  }
+  if (feet > 2500) {
+    greeting = 'No can do.'
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return "Thank you so much."
+  }
+  if (tip === 'not as generous') {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
+}
